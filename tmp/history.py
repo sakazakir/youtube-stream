12586787416f07958f3f57c5352f2bbc76bb24f2
ryouiti <5118037s@st.toho-u.ac.jp>
2022-05-30 16:23:52 +0900

    first

---
+++ tmp/history.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+for i in range(10):
+    print(i)
+    
+for i in range(10):
+    print(i)
+    
+for i in range(10):
+    print(i)
+    
+get_ipython().run_line_magic('history', '')
